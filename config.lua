85fc52ce6f51d2c0827f886c5d7532b0016dfa90
ricx <66686454+zelbeus@users.noreply.github.com>
2021-05-24 20:16:50 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,49 +1,48 @@
 Config = {}
 
-Config.KeyID = {['key'] = 0xD8F73058, ['name'] = 'U'} -- U button
-Config.AtMissionText = "Press "..Config.KeyID['name'].." to start"
+Config.KeyID = {['key'] = 0xD8F73058, ['name'] = 'U'} -- Add the control and the text name
+Config.AtMissionText = "Press "..Config.KeyID['name'].." to start" --says 'Press U to start' default. Change KeyID['name'] to change the control name
 
 Config.D1 = {
-	["JobBlipName"] = "Job: Delivery",
-	["JobBlipSprite"] = -426139257,
-	["JobBlipCoords"] = 
+	["JobBlipName"] = "Job: Delivery", --Blip Name on Map
+	["JobBlipSprite"] = -426139257, -- Blip Sprite
+	["JobBlipCoords"] = --Mission Start and Map Blip Coords
 	{
-		["X"] = 2790.46, --2790.46, -1421.77, 45.39
+		["X"] = 2790.46,
 		["Y"]= -1421.77, 
 		["Z"] = 45.39
 	},
-	["Reward"] = 10,
-	["XP"] = 1,
-	["MissionBlip"] = -984192463,
-	["MissionBlipName"] = "Delivery here!",
-	["JobName"] = "St Denis Job",
-	["JobStartText"] = 'Job Started!~n~~o~~h~Deliver the package! Check your map!',
+	["Reward"] = 10, --Reward Money, change to any int
+	["XP"] = 1, --Reward XP, change to any int
+	["MissionBlip"] = -984192463, --Blip Sprite for drop position's blip on map
+	["MissionBlipName"] = "Delivery here!", --Drop' Blip Name on Map
+	["JobName"] = "St Denis Job",--Name to display for notification
+	["JobStartText"] = 'Job Started!~n~~o~~h~Deliver the package! Check your map!', --Help text after the job started
 	["StartTextDuration"] = 3, -- seconds
-	["AlreadyInJobText"] = '~e~You have to finish the current~h~~t6~delivery~e~!',
+	["AlreadyInJobText"] = '~e~You have to finish the current~h~~t6~delivery~e~!',--Help text when the player wants to start again the mission after started
 	["AlreadyTextDuration"] = 3, -- seconds
-	["HasWeaponText"] = '~h~~e~Put your weapon away!',
+	["HasWeaponText"] = '~h~~e~Put your weapon away!',--When the player is not unarmed at starting the job
 	["HasWeaponTextDuration"] = 3, -- seconds
-	["FailedText"] = '~h~~e~You lost the package!',
+	["FailedText"] = '~h~~e~You lost the package!',--Help Text when the player is dead/swimming/climbing/falling
 	["FailedTextDuration"] = 3, -- seconds
 }
 
-Config.D1Prop = { 
-	["PropID"] = `p_chair_crate02x`,
-	["BoneID"] = 131,
-	["PX"] = -0.05,
-	["PY"] = 0.45,
-	["PZ"] = 0.08,
-	["PRX"] = 90.0,
-	["PRY"] = 90.0,
-	["PRZ"] = 80.0,
+Config.D1Prop = { --Delivery box prop
+	["PropID"] = `p_chair_crate02x`, --prop hash
+	["BoneID"] = 131, --Male bone id
+	["PX"] = -0.05, --Male bone X pos
+	["PY"] = 0.45,--Male bone Y pos
+	["PZ"] = 0.08,--Male bone Z pos
+	["PRX"] = 90.0, --Male bone X rotate
+	["PRY"] = 90.0,--Male bone Y rotate
+	["PRZ"] = 80.0,--Male bone Z rotate
 }
 
-Config.D1MAXROUTE = 3
 
-Config.D1DELIVERY = {
+Config.D1DELIVERY = {--Deliveries. Easy to add, just get the x,y,z coords and create more positions.
 	[1] = {
-		["Odds"] = 1.5,
-		["CP"] = {x=2831.71, y=-1414.59, z=45.39},
+		["Odds"] = 1.5,--Reward odds
+		["CP"] = {x=2831.71, y=-1414.59, z=45.39},--Delivery position
 	},
 	[2] = {
 		["Odds"] = 1.5,
@@ -52,5 +51,5 @@ Config.D1DELIVERY = {
 	[3] = {
 		["Odds"] = 1.7,
 		["CP"] = {x=2861.75, y=-1372.91, z=44.56},
-	} -- MAXROUTE = 3
+	}
 }
